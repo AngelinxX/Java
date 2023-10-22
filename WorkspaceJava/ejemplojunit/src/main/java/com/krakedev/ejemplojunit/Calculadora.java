120a83297b5be016da0947fd4afb4acb827f52a2
AngelinxX <fajardo_aa@outlook.com>
2023-10-22 15:39:44 -0500

    Video reto 30

---
+++ WorkspaceJava/ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,15 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	public int sumar(int valor1, int valor2) {
+		int total;
+		total = valor1+valor2;
+		return total;
+	}
+	
+	public int restar(int valor1, int valor2) {
+		int total;
+		total = valor1-valor2;
+		return total;
+	}
+}
