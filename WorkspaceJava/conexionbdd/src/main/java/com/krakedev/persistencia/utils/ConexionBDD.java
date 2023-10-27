1e66d7eab2d90b8d0a7df36d8176140d2289e247
Luis Jacome <luis.jacome@cobistopaz.com>
2023-10-27 06:47:32 -0500

    Ejercicio BDD

---
+++ WorkspaceJava/conexionbdd/src/main/java/com/krakedev/persistencia/utils/ConexionBDD.java
@@ -0,0 +1,24 @@
+package com.krakedev.persistencia.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBDD {
+	private final static String DRIVER="org.postgresql.Driver";
+	private final static String URL="jdbc:postgresql://localhost:5432/postgres";
+	private final static String USUARIO="postgres";
+	private final static String CLAVE="Ceydis.159783";
+	public static Connection conectar(){
+		Connection connection = null;
+		try {
+			Class.forName(DRIVER);
+			connection = DriverManager.getConnection(URL, USUARIO,CLAVE);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
